f97f0f49bcdb7f80de3c00d6a4d52a26b0cf6707
Junior Melo <alvasmj@hotmail.com>
2023-02-24 16:56:47 -0300

    Atualização nos ícones

--- js/main.js
+++ js/main.js
@@ -1,52 +1,54 @@
-const newTaskField = document.querySelector('#newTaskField')
-const newTaskBtn = document.querySelector('#newTaskAdd')
+const newTaskField = document.querySelector("#newTaskField");
+const newTaskBtn = document.querySelector("#newTaskAdd");
 
 const validateField = () => {
-    if (newTaskField.value.trim() == "") {
-        return false
-    }
-    return true
-}
+  if (newTaskField.value.trim() == "") {
+    return false;
+  }
+  return true;
+};
 
-const tasksContainer = document.querySelector('.tasksContainer')
+const tasksContainer = document.querySelector(".tasksContainer");
 
-newTaskBtn.addEventListener('click', () => {
-    if (!validateField()) {
-        newTaskField.style.border = "2px solid red"
-        newTaskField.focus()
-        alert('Favor preencher corretamente o campo!')
-    } else {
-        const taskField = document.createElement('div')
-        taskField.classList.add('taskField')
-        const task = document.createElement('p')
-        task.innerText = newTaskField.value
-        task.classList.add('task')
-        const iconsField = document.createElement('div')
-        iconsField.classList.add('iconsField')
-        const checkIcon = document.createElement('i')
-        checkIcon.classList.add('fa-regular')
-        checkIcon.classList.add('fa-circle-check')
-        const removeIcon = document.createElement('i')
-        removeIcon.classList.add('fa-solid')
-        removeIcon.classList.add('fa-trash')
-        tasksContainer.appendChild(taskField)
-        taskField.appendChild(task)
-        taskField.appendChild(iconsField)
-        iconsField.appendChild(checkIcon)
-        iconsField.appendChild(removeIcon)
-        newTaskField.focus()
-        newTaskField.value = ""
-        newTaskField.style.border = "2px solid black"
-    }
-})
+newTaskBtn.addEventListener("click", () => {
+  if (!validateField()) {
+    newTaskField.style.border = "2px solid red";
+    newTaskField.focus();
+  } else {
+    const taskField = document.createElement("div");
+    taskField.classList.add("taskField");
+    const task = document.createElement("p");
+    task.innerText = newTaskField.value;
+    task.classList.add("task");
+    const iconsField = document.createElement("div");
+    iconsField.classList.add("iconsField");
+    const checkBtn = document.createElement("button");
+    checkBtn.classList.add("checkBtn");
+    const checkIcon = document.createElement("i");
+    checkIcon.classList.add("fa-regular");
+    checkIcon.classList.add("fa-circle-check");
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("removeBtn");
+    const removeIcon = document.createElement("i");
+    removeIcon.classList.add("fa-solid");
+    removeIcon.classList.add("fa-trash");
+    tasksContainer.appendChild(taskField);
+    taskField.appendChild(task);
+    taskField.appendChild(iconsField);
+    iconsField.appendChild(checkBtn);
+    checkBtn.appendChild(checkIcon);
+    iconsField.appendChild(removeBtn);
+    removeBtn.appendChild(removeIcon);
+    newTaskField.focus();
+    newTaskField.value = "";
+    newTaskField.style.border = "2px solid black";
+  }
+});
 
-newTaskField.addEventListener('change', () => {
-    newTaskField.style.border = "2px solid black"
-})
+newTaskField.addEventListener("change", () => {
+  newTaskField.style.border = "2px solid black";
+});
 
-const checkIcon = document.querySelector('.iconsField')
-checkIcon.addEventListener('click', () => {
-    console.log('Clicou')
-})
-
-
+document.querySelector(".checkBtn").addEventListener("click", () => {
+  console.log("Clicou");
+});
