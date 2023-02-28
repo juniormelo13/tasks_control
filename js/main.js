61d90cd9f40128353f787008cec592fe53edcc44
juniormelo13 <alvasmj@hotmail.com>
2023-02-28 19:58:19 -0300

    Configurado botões de fechamento, cancelamento, limpeza e adição do valor do input

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 const newTaskInput = document.querySelector("#newTaskInput");
 const newTaskBtn = document.querySelector("#newTaskBtn");
-const resetBtn = document.querySelector('#resetBtn')
+const resetBtn = document.querySelector("#resetBtn");
 
-resetBtn.addEventListener('click', () => {
-  newTaskInput.value = ''
-  newTaskInput.focus()
-})
+resetBtn.addEventListener("click", () => {
+  newTaskInput.value = "";
+  newTaskInput.focus();
+});
 
 const tasksContainer = document.querySelector(".tasksContainer");
-const editField = document.querySelector('.editField')
-const header = document.querySelector('#header')
-const mainContainer = document.querySelector('#mainContainer')
+const editField = document.querySelector(".editField");
+const header = document.querySelector("#header");
+const mainContainer = document.querySelector("#mainContainer");
 
 const validateField = () => newTaskInput.value.trim() != "";
 
@@ -32,32 +32,32 @@ newTaskBtn.addEventListener("click", () => {
     const checkIcon = document.createElement("i");
     checkIcon.classList.add("fa-solid");
     checkIcon.classList.add("fa-check");
-    checkBtn.setAttribute('title', 'Concluir')
+    checkBtn.setAttribute("title", "Concluir");
     checkBtn.addEventListener("click", () =>
       completeClick(taskField, taskContent)
     );
-    
+
     const scheduleBtn = document.createElement("button");
     scheduleBtn.classList.add("scheduleBtn");
     const scheduleIcon = document.createElement("i");
     scheduleIcon.classList.add("fa-solid");
     scheduleIcon.classList.add("fa-calendar-days");
-    scheduleBtn.setAttribute('title', 'Agendar')
-    
+    scheduleBtn.setAttribute("title", "Agendar");
+
     const editBtn = document.createElement("button");
     editBtn.classList.add("editBtn");
     const editIcon = document.createElement("i");
     editIcon.classList.add("fa-solid");
     editIcon.classList.add("fa-file-pen");
-    editBtn.setAttribute('title', 'Editar')
-    editBtn.addEventListener('click', () => editClick(taskField, taskContent))
+    editBtn.setAttribute("title", "Editar");
+    editBtn.addEventListener("click", () => editClick(taskContent));
 
     const removeBtn = document.createElement("button");
     removeBtn.classList.add("removeBtn");
     const removeIcon = document.createElement("i");
     removeIcon.classList.add("fa-solid");
     removeIcon.classList.add("fa-trash");
-    removeBtn.setAttribute('title', 'Excluir')
+    removeBtn.setAttribute("title", "Excluir");
     removeBtn.addEventListener("click", () =>
       deleteClick(taskField, taskContent)
     );
@@ -68,9 +68,9 @@ newTaskBtn.addEventListener("click", () => {
     iconsField.appendChild(checkBtn);
     checkBtn.appendChild(checkIcon);
     iconsField.appendChild(scheduleBtn);
-    scheduleBtn.appendChild(scheduleIcon)
-    iconsField.appendChild(editBtn)
-    editBtn.appendChild(editIcon)
+    scheduleBtn.appendChild(scheduleIcon);
+    iconsField.appendChild(editBtn);
+    editBtn.appendChild(editIcon);
     iconsField.appendChild(removeBtn);
     removeBtn.appendChild(removeIcon);
 
@@ -89,24 +89,53 @@ const completeClick = (taskField, taskContent) => {
   newTaskInput.focus();
 };
 
-const editInput = document.querySelector('#editInput')
+const editInput = document.querySelector("#editInput");
 
-const editClick = (taskField, taskContent) => {
-  editField.style.display = 'block'
-  header.style.pointerEvents = "none"
-  mainContainer.style.pointerEvents = "none"
-  editInput.focus()
+const editClick = (taskContent) => {
+  editField.style.display = "block";
+  header.style.pointerEvents = "none";
+  mainContainer.style.pointerEvents = "none";
+  editInput.focus();
 
-  const tasks = tasksContainer.childNodes
+  const tasks = tasksContainer.childNodes;
 
   for (const task of tasks) {
-    if(task.firstChild.isSameNode(taskContent)) {
-      editInput.value = taskContent.innerText
-      
+    if (task.firstChild.isSameNode(taskContent)) {
+      editInput.value = taskContent.innerText;
     }
   }
-  
+};
+
+const editFieldCloseBtn = document.querySelector('#editFieldCloseBtn')
+const cancelEditBtn = document.querySelector('#cancelEditBtn')
+const resetEditBtn = document.querySelector('#resetEditBtn')
+
+const closeEditField = () => {
+  editField.style.display = "none";
+  header.style.pointerEvents = "auto";
+  mainContainer.style.pointerEvents = "auto";
 }
+
+editFieldCloseBtn.addEventListener('click', closeEditField)
+cancelEditBtn.addEventListener('click', closeEditField)
+
+resetEditBtn.addEventListener("click", () => {
+  editInput.value = "";
+  editInput.focus();
+});
+
+const validateEditField = () => editInput.value.trim() != "";
+
+const confirmEditBtn = document.querySelector('#confirmEditBtn')
+
+confirmEditBtn.addEventListener('click', () => {
+  if (!validateEditField()) {
+    console.log('Erro!')
+  } else {
+    
+  }
+  
+})
 
 const deleteClick = (taskField, taskContent) => {
   const tasks = tasksContainer.childNodes;
@@ -118,5 +147,4 @@ const deleteClick = (taskField, taskContent) => {
   newTaskInput.focus();
 };
 
-
-const scheduleField = document.querySelector('.scheduleField')
+const scheduleField = document.querySelector(".scheduleField");
