3997d1b679c0e9dab1310f0e8818cbd9e50690c0
Junior Melo <alvasmj@hotmail.com>
2023-03-02 16:07:37 -0300

    Realizado identação do código, adicionado comentários, configurado campo de edição

--- js/main.js
+++ js/main.js
@@ -1,32 +1,49 @@
+// Input principal para descrição das tarefas
 const newTaskInput = document.querySelector("#newTaskInput");
-const newTaskBtn = document.querySelector("#newTaskBtn");
-const resetBtn = document.querySelector("#resetBtn");
+newTaskInput.focus();
 
+// Botão para limpar o input principal
+const resetBtn = document.querySelector("#resetBtn");
 resetBtn.addEventListener("click", () => {
   newTaskInput.value = "";
   newTaskInput.focus();
 });
 
+// Campo onde as novas tarefas serão adicionadas
 const tasksContainer = document.querySelector(".tasksContainer");
 
+// Botão para adicionar a nova tarefa
+const newTaskBtn = document.querySelector("#newTaskBtn");
 newTaskBtn.addEventListener("click", () => {
+  // Validação do input principal
   const validateField = () => newTaskInput.value.trim() != "";
-
   if (!validateField()) {
-    console.log("Erro");
+    // Caso o valor do input seja inválido: Será adicionado a class abaixo no input
+    newTaskInput.classList.add("newTaskInputError");
   } else {
+    // Caso o valor do input seja válido: Criação dos parágrafos e botões referentes a cada tarefa adicionada
+
+    // Tarefa
     const taskField = document.createElement("div");
+    tasksContainer.appendChild(taskField);
     taskField.classList.add("taskField");
 
+    // Texto da tarefa
     const taskContent = document.createElement("p");
+    taskField.appendChild(taskContent);
     taskContent.innerText = newTaskInput.value;
 
+    // Campo dos botões/ícones
     const iconsField = document.createElement("div");
+    taskField.appendChild(iconsField);
     iconsField.classList.add("iconsField");
 
+    // Botão para conclusão a tarefa
     const checkBtn = document.createElement("button");
+    iconsField.appendChild(checkBtn);
     checkBtn.classList.add("checkBtn");
     const checkIcon = document.createElement("i");
+    checkBtn.appendChild(checkIcon);
     checkIcon.classList.add("fa-solid");
     checkIcon.classList.add("fa-check");
     checkBtn.setAttribute("title", "Concluir");
@@ -34,118 +51,118 @@ newTaskBtn.addEventListener("click", () => {
       completeClick(taskField, taskContent)
     );
 
+    // Botão para agendamento da tarefa
     const scheduleBtn = document.createElement("button");
+    iconsField.appendChild(scheduleBtn);
     scheduleBtn.classList.add("scheduleBtn");
     const scheduleIcon = document.createElement("i");
+    scheduleBtn.appendChild(scheduleIcon);
     scheduleIcon.classList.add("fa-solid");
     scheduleIcon.classList.add("fa-calendar-days");
     scheduleBtn.setAttribute("title", "Agendar");
 
+    //Botão para edição da tarefa
     const editBtn = document.createElement("button");
+    iconsField.appendChild(editBtn);
     editBtn.classList.add("editBtn");
     const editIcon = document.createElement("i");
+    editBtn.appendChild(editIcon);
     editIcon.classList.add("fa-solid");
     editIcon.classList.add("fa-file-pen");
     editBtn.setAttribute("title", "Editar");
     editBtn.addEventListener("click", () => editClick(taskContent));
 
+    // Botão para exclusão da tarefa
     const removeBtn = document.createElement("button");
+    iconsField.appendChild(removeBtn);
     removeBtn.classList.add("removeBtn");
+    removeBtn.appendChild(removeIcon);
     const removeIcon = document.createElement("i");
     removeIcon.classList.add("fa-solid");
     removeIcon.classList.add("fa-trash");
     removeBtn.setAttribute("title", "Excluir");
-    removeBtn.addEventListener("click", () =>
-      deleteClick(taskField, taskContent)
-    );
-
-    tasksContainer.appendChild(taskField);
-    taskField.appendChild(taskContent);
-    taskField.appendChild(iconsField);
-    iconsField.appendChild(checkBtn);
-    checkBtn.appendChild(checkIcon);
-    iconsField.appendChild(scheduleBtn);
-    scheduleBtn.appendChild(scheduleIcon);
-    iconsField.appendChild(editBtn);
-    editBtn.appendChild(editIcon);
-    iconsField.appendChild(removeBtn);
-    removeBtn.appendChild(removeIcon);
+    removeBtn.addEventListener("click", () => deleteClick(taskField));
 
     newTaskInput.focus();
-    newTaskInput.value = ""; 
+    newTaskInput.value = "";
   }
 });
 
+// Configuração do botão para conclusão da tarefa
 const completeClick = (taskField, taskContent) => {
-  const tasks = tasksContainer.childNodes;
-  for (const task of tasks) {
-    if (task.firstChild.isSameNode(taskContent)) {
-      console.log("Completou");
-    }
-  }
+  taskContent.classList.toggle("completed");
+  taskField.classList.toggle("completed");
   newTaskInput.focus();
 };
 
+// Cabeçalho da aplicação
 const header = document.querySelector("#header");
-const mainContainer = document.querySelector("#mainContainer");
-const editField = document.querySelector(".editField");
-const editInput = document.querySelector("#editInput");
-const confirmEditBtn = document.querySelector("#confirmEditBtn");
-const closeEditFieldBtn = document.querySelector("#closeEditFieldBtn");
-const cancelEditBtn = document.querySelector("#cancelEditBtn");
-const resetEditInputBtn = document.querySelector("#resetEditInputBtn");
 
+// Conteúdo principal da aplicação
+const mainContainer = document.querySelector("#mainContainer");
+
+// Janela para edição das tarefas
+const editField = document.querySelector(".editField");
+
+// Input de texto para edição das tarefas
+const editInput = document.querySelector("#editInput");
+
+// Função responsável pela abertura da janela de edição
 const editClick = (taskContent) => {
   editField.style.display = "block";
   header.style.pointerEvents = "none";
   mainContainer.style.pointerEvents = "none";
-  editInput.focus();
 
-  const tasks = tasksContainer.childNodes;
+  editInput.value = taskContent.innerText;
+  editInput.select();
 
-  for (const task of tasks) {
-    if (task.firstChild.isSameNode(taskContent)) {
-      editInput.value = taskContent.innerText;
-    }
-  }
+  taskContent.classList.add("task");
 };
 
-confirmEditBtn.addEventListener("click", () => {
-  const validateEditField = () => editInput.value.trim() != "";
-  if (!validateEditField()) {
-    console.log("Erro!");
-  } else {
-    // taskContent.innerText = editInput.value;
-    editField.style.display = "none";
-    header.style.pointerEvents = "auto";
-    mainContainer.style.pointerEvents = "auto";
-    console.log("editou");
-  }
-});
-
+// Função responsável pelo fechamento da janela de edições
 const closeEditField = () => {
   editField.style.display = "none";
   header.style.pointerEvents = "auto";
   mainContainer.style.pointerEvents = "auto";
-  console.log("fechou");
+
+  document.querySelector(".task").classList.remove("task");
 };
 
+// Adicionado a função de fechamento nos botões de fechar "X" e "Cancelar"
+const closeEditFieldBtn = document.querySelector("#closeEditFieldBtn");
 closeEditFieldBtn.addEventListener("click", closeEditField);
 
+const cancelEditBtn = document.querySelector("#cancelEditBtn");
+cancelEditBtn.addEventListener("click", closeEditField);
+
+// Configuração do botão de limpar o input do campo de edição
+const resetEditInputBtn = document.querySelector("#resetEditInputBtn");
 resetEditInputBtn.addEventListener("click", () => {
   editInput.value = "";
   editInput.focus();
-  console.log("limpou");
 });
 
-cancelEditBtn.addEventListener("click", closeEditField);
+// Configuração do botão de confirmação da edição
+const confirmEditBtn = document.querySelector("#confirmEditBtn");
+confirmEditBtn.addEventListener("click", () => {
+  // Função para validar o input do campo de edição
+  const validateEditField = () => editInput.value.trim() != "";
+  if (!validateEditField()) {
+    // Caso o valor do input seja inválido: Será adicionado a class abaixo no input
+    editInput.classList.add("editInputError");
+  } else {
+    // Caso o valor do input seja válido: Será realizado a edição da tarefa conforme config. abaixo
+    editField.style.display = "none";
+    header.style.pointerEvents = "auto";
+    mainContainer.style.pointerEvents = "auto";
 
-const deleteClick = (taskField, taskContent) => {
-  const tasks = tasksContainer.childNodes;
-  for (const task of tasks) {
-    if (task.firstChild.isSameNode(taskContent)) {
-      taskField.remove();
-    }
+    document.querySelector(".task").innerText = editInput.value;
+    document.querySelector(".task").classList.remove("task");
   }
+});
+
+// Configuração do botão de deleção da tarefa
+const deleteClick = (taskField) => {
+  taskField.remove();
   newTaskInput.focus();
 };
