33b57e969077191df1d86cebec72d5a431c3f4ec
Junior Melo <alvasmj@hotmail.com>
2025-10-09 21:40:37 -0300

    Adicionado comentários no código para facilitar o entendimento e futuras manutenções

---
+++ js/modules/menu.js
@@ -0,0 +1,76 @@
+// Funções e variáveis para o funcionamento do menu.
+
+// Importações.
+import { nameInput, resetBtnNameInput } from "./username.js";
+import { confirmationWindow } from "./auxiliary-func-for-window.js";
+
+const menuBtn = document.querySelector("#menuButton"); // Botão para abrir e fechar o menu.
+export const menu = document.querySelector("#menu"); // Menu.
+const menuBtnIcon = document.querySelector("#menuButtonIcon"); // Ícone dentro do botão do menu.
+const menuBtnIconMobile = document.querySelector("#menuButtonIconMobile"); // Ícone dentro do botão do menu para celular.
+export let menuOpen = false; // Variável para guardar o status do menu (aberto ou fechado), a aplicação inicia com o menu fechado.
+
+// Função responsável por alternar características do botão ao abrir ou fechar menu. 
+function menuBtnToggle() {
+  menuBtnIcon.classList.toggle("fa-angles-down");
+  menuBtnIcon.classList.toggle("fa-xmark");
+  menuBtnIconMobile.classList.toggle("fa-angles-left");
+  menuBtnIconMobile.classList.toggle("fa-xmark");
+  menuBtn.classList.toggle("active");
+}
+
+// Função responsável por abrir o menu.
+function menuShow() {
+  menuOpen = !menuOpen;
+  if (localStorage.getItem("infoAccountName")) {
+    nameInput.value = localStorage.getItem("infoAccountName");
+    resetBtnNameInput()
+  } else {
+    nameInput.value = ""
+    resetBtnNameInput()
+  }
+  menu.classList.remove("pointerEventsNone");
+  menu.classList.remove("hide");
+  menu.classList.remove("menuVanish");
+  menu.classList.add("menuAppear");
+  menuBtnToggle();
+  menuBtn.disabled = false;
+}
+
+// Função responsável por fechar o menu.
+function menuHide() {
+  menuOpen = !menuOpen;
+  menu.classList.add("pointerEventsNone");
+  menu.classList.remove("menuAppear");
+  menu.classList.add("menuVanish");
+  menuBtn.disabled = true;
+  menuBtnToggle();
+  setTimeout(() => {
+    menu.classList.add("hide");
+    menuBtn.disabled = false;
+  }, 200);
+}
+
+// Função principal responsável pelo funcionamento do menu.
+export default function initDropdownMenu() {
+
+  // Abrir e fechar o menu ao clicar no botão.
+  menuBtn.addEventListener("click", () => {
+    if (!menuOpen) {
+      menuShow();
+    } else {
+      menuHide();
+    }
+  });
+
+  // Configuração para fechar o menu ao clicar em qualquer local da tela.
+  document.addEventListener("mousedown", (e) => {
+    if (!menu.contains(e.target) && !menuBtn.contains(e.target) && !menuBtnIcon.contains(e.target) && !confirmationWindow.contains(e.target) && menuOpen && !confirmationWindow.classList.contains("appearWindow")) {
+      menuHide();
+    } else if (confirmationWindow.classList.contains("appearWindow") && menuOpen && !menu.contains(e.target) && !menu.classList.contains("menuBlur")) {
+      menuHide();
+    }
+  })
+  
+}
+
---
+++ js/modules/theme.js
@@ -0,0 +1,43 @@
+import { scheduleInputDate, scheduleInputTime } from "./task-schedule.js";
+import { removeAllConfigBtn, checkRemoveAllConfigBtn } from "./remove-all-config.js";
+import { enableBtn } from "./auxiliary-func-for-btn.js";
+
+// Funções responsáveis pela troca de tema da aplicação
+
+const html = document.querySelector("html"); // Selecão do HTML
+const themeCheckBox = document.querySelector("#themeCheckBox"); // Seleção do input checkbox, que muda o tema ao receber o clique do usuário.
+
+// Função responsável por alterar o tema para escuro.
+export function toDarkTheme() {
+  html.classList.add("darkTheme");
+  scheduleInputDate.classList.add("darkTheme");
+  scheduleInputTime.classList.add("darkTheme");
+  localStorage.setItem("theme", "darkTheme");
+  themeCheckBox.checked = true;
+  if (removeAllConfigBtn.disabled) {
+    enableBtn(removeAllConfigBtn);
+  }
+}
+
+// Função responsável por alterar o tema para claro.
+export function toLightTheme() {
+  html.classList.remove("darkTheme");
+  scheduleInputDate.classList.remove("darkTheme");
+  scheduleInputTime.classList.remove("darkTheme");
+  localStorage.removeItem("theme");
+  themeCheckBox.checked = false;
+}
+
+// Função principal responsável pela troca de temas pelo usuário.
+export default function initChangeTheme() {
+    
+  themeCheckBox.addEventListener("change", () => {
+    if (!html.classList.contains("darkTheme")) {
+      toDarkTheme();
+    } else {
+      toLightTheme();
+      checkRemoveAllConfigBtn();
+    }
+  });
+
+}
