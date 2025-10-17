a81622f5e456049ec4c9a9748568c7eaba50e954
Junior Melo <alvasmj@hotmail.com>
2025-10-17 00:11:05 -0300

    Adicionado comentários no código do arquivo de configuração principal do babel

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
 // Configuração da ferramenta Babel.
 
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env', // Preset inteligente que informa as regras de transpilação.
-      {
-        targets: '> 0.25%, not dead', // Traduza o código para que ele seja compatível com navegadores que tenham mais de 0.25% de participação no mercado global e que não estejam 'mortos'.
+module.exports = { // Exporta o objeto de configuração para o Babel.
+  presets: [ // Define um array de presets (conjuntos de plugins) a serem usados.
+    [ // Inicia a configuração para um preset específico.
+      '@babel/preset-env', // Usa o preset inteligente que transpila o JS moderno com base nos alvos.
+      { // Objeto de opções para o '@babel/preset-env'.
+        targets: '> 0.25%, not dead', // Define os navegadores alvo para a transpilação.
       },
     ],
   ],
